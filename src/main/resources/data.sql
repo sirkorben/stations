38ae292650c490b4877652b0a469525d70b57b6f
sirkorben <vejlevas@gmail.com>
2023-02-06 11:08:42 +0200

    switching to H2 db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO mobile_station(id, uuid, last_knownx, last_knowny) VALUES (1, "98ff19a0-cf34-48f8-9a02-7a87b6469ceg", 29, 6);
-INSERT INTO mobile_station(id, uuid, last_knownx, last_knowny) VALUES (2, "98ff19a0-cf34-48f8-9a02-7a87b6444cag"), 5, 6);
-INSERT INTO mobile_station(id, uuid, last_knownx, last_knowny) VALUES (3, "98ff19a0-cf34-48f8-9a02-7a87b6666cig"), 14, 7);
+INSERT INTO mobile_station(mobile_station_id, last_knownx, last_knowny, error_radius) VALUES ('2D1EBC5B7D2741979CF0E84451C5BBB1', 29, 6, 0);
+
+INSERT INTO base_station(base_station_id, coordinatex, coordinatey, name, detection_radius) VALUES ('04D2F1E464B14268A3EC7A080B474F65', 11, 11, 'stationA', 6);
+INSERT INTO base_station(base_station_id, coordinatex, coordinatey, name, detection_radius) VALUES ('DD4A0E89FD6F41A2804E5A4A1801AE8E', 18, 16, 'stationB', 6);
+INSERT INTO base_station(base_station_id, coordinatex, coordinatey, name, detection_radius) VALUES ('5A540269CB0C42BD8A12AAD9B47C869B', 18.83, 7.44, 'stationC', 6);
